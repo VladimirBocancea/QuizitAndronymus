8f108442ac836beee925dc5ceebb8c2f1c16cdc8
vlad <vladimirbocancea@gmail.com>
2015-03-18 22:46:46 +0100

    sobrecarga memoria pero funciona splash y preguntas

--- app/src/main/java/com/andronymus/quizit/Splash.java
+++ app/src/main/java/com/andronymus/quizit/Splash.java
@@ -1,7 +1,6 @@
 package com.andronymus.quizit;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.ActionBarActivity;
@@ -17,11 +16,35 @@ import java.util.TimerTask;
 import static android.view.Window.FEATURE_NO_TITLE;
 
 
-public class Splash extends Activity {
+public class Splash extends ActionBarActivity {
     //duration of the splash screen
-    private static final long SPLASH_SCREEN = 4000;
+    private static final long SPLASH_SCREEN = 3000;
     private ProgressBar progressBar;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        progressBar = (ProgressBar) findViewById(R.id.progressBarSplash);
 
+        //requestWindowFeature(FEATURE_NO_TITLE);
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN);
+
+    }
+
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            Intent mainIntent = new Intent().setClass(Splash.this, MainActivity.class);
+
+            startActivity(mainIntent);
+            finish();
+
+        }
+    };
 
 }
